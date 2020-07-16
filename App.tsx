06661b2e9282de0395b9f41ffe7118fcac752d94
Renato Rocha <renatob.rocha@gmail.com>
2020-07-15 21:28:33 -0300

    Add animation to logo

--- App.tsx
+++ App.tsx
@@ -1,13 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef } from 'react';
+import { StyleSheet, View } from 'react-native';
+import Animated, { Clock, Easing, block, cond, not, set, eq } from "react-native-reanimated";
 
 import Logo from "./Logo";
 
+const {timing, useCode, startClock, clockRunning} = Animated;
+
+const runProgress = (clock: Clock) => {
+  const state = { 
+    finished: new Animated.Value(0), 
+    position: new Animated.Value(0), 
+    frameTime: new Animated.Value(0), 
+    time: new Animated.Value(0) 
+  };
+
+  const config = { 
+    toValue: new Animated.Value(1), 
+    duration: 3000, 
+    easing: Easing.inOut(Easing.ease)
+  };
+
+  return block([
+    timing(clock, state, config),
+    cond(eq(state.finished, 1), [
+      set(state.finished, 0),
+      set(state.frameTime, 0),
+      set(state.time, 0),
+      set(config.toValue, not(state.position))
+    ]),
+    state.position
+  ])
+  
+}
+
 export default function App() {
+  const clock = useRef(new Clock()).current;
+  const progress = useRef(new Animated.Value(5)).current;
+
+  useCode(() => [startClock(clock), set(progress, runProgress(clock))], []);
+
   return (
     <View style={styles.container}>
-      <Logo />
+      <Logo progress={progress}/>
       <StatusBar style="auto" />
     </View>
   );
